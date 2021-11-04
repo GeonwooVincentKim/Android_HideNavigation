7f4d12afb983d38b98a55606372269d6f8c6cd4f
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-04 15:21:03 +0900

    [Testing]

--- app/src/main/java/com/example/fullscreenmode_application/SecondFragment.kt
+++ app/src/main/java/com/example/fullscreenmode_application/SecondFragment.kt
@@ -1,6 +1,8 @@
 package com.example.fullscreenmode_application
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.Gravity
@@ -47,19 +49,69 @@ class SecondFragment : Fragment() {
 
 //        createToast()
 
+//        val customSnackBarView = layoutInflater.inflate(R.layout.cus)
 
         val secondFragmentView: ConstraintLayout = view.findViewById(R.id.fragment_second_fragment)
         secondFragmentView.setOnClickListener {
-            val snack = Snackbar.make(it,"This is a simple Snackbar", Snackbar.LENGTH_LONG)
-            snack.show()
-            findNavController().navigate(R.id.action_secondFragment_to_first_fragment)
+            createSnackBar(it)
+//            val customSnackBar: Snackbar = Snackbar.make(con)
+//            findNavController().navigate(R.id.action_secondFragment_to_first_fragment)
         }
 
         return view
     }
 
+    @SuppressLint("InflateParams")
+    private fun createSnackBar(it: View) {
+//        val toastText: TextView = it.findViewById(R.id.toastText)
+//
+//        val stringResult = "Very Good"
+//        toastText.text = stringResult.lowercase(Locale.getDefault())
+        val view1 = layoutInflater.inflate(R.layout.toast_border, null)
+        view1.setBackgroundResource(android.R.drawable.toast_frame)
+        view1.foregroundGravity = Gravity.CENTER
+        val toastText: TextView = view1.findViewById(R.id.toastText)
+
+        toastText.textSize = 20F
+//        toastText.setTextColor(Color.BLACK)
+
+        val stringResult = "Very Good"
+        toastText.text = stringResult.lowercase(Locale.getDefault())
+
+
+//        val snack = Snackbar.make(it, "This is a simple Snackbar", Snackbar.LENGTH_LONG)
+        val snack = Snackbar.make(it, toastText.text, Snackbar.LENGTH_LONG)
+//            .setBackgroundTint(Color.GRAY)
+//            .setTextColor(Color.WHITE)
+
+        val snackBarView: View = snack.view
+        snackBarView.setBackgroundColor(Color.YELLOW)
+        snack.setTextColor(Color.BLACK)
+        snackBarView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+//        val toastText: TextView = snackBarView.findViewById(R.id.toastText)
+//        toastText.textAlignment = View.TEXT_ALIGNMENT_CENTER
+//        toastText.textSize = 20F
+////        toastText.setTextColor(Color.BLACK)
+//
+//        val stringResult = "Very Good"
+//        toastText.text = stringResult.lowercase(Locale.getDefault())
+
+//        val countDownTimer = object : CountDownTimer(5000, 1000) {
+//            override fun onTick(millisUntilFinished: Long) {}
+//            override fun onFinish() {
+//            }
+//        }
+//        toastText.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+//        toastText.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        snack.duration = 4000
+
+        snack.show()
+    }
+
     @SuppressLint("InflateParams", "SetTextI18n")
-    private fun createToast(){
+    private fun createToast() {
         val view1 = layoutInflater.inflate(R.layout.toast_border, null)
         view1.setBackgroundResource(android.R.drawable.toast_frame)
         val toastText: TextView = view1.findViewById(R.id.toastText)
